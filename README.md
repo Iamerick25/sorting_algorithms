23c0f798b8e5754283bca2210e55a444b4a65066
Iamerick25 <erickchinyem@gmail.com>
2023-12-14 23:37:48 -0800

    First commit for the project

---
+++ README.md
@@ -0,0 +1,1 @@
+## This is a 0x1B. C - Sorting algorithms & Big O project
---
+++ deck.h
@@ -0,0 +1,39 @@
+#ifndef DECK_h
+#define DECK_H
+
+typedef enum kind_e
+{
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+    const char *value;
+    const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+    const card_t *card;
+    struct deck_node_s *prev;
+    struct deck_node_s *next;
+} deck_node_t;
+
+#endif /** DECK_H */
